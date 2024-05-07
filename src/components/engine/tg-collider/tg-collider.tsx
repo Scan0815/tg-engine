161b67003216e5f476c52094e6cf9236e78dcb14
oliver <oliver@bitcaster.de>
2024-05-07 17:05:49 +0200

    update collider add width and height and offsets

--- src/components/engine/tg-collider/tg-collider.tsx
+++ src/components/engine/tg-collider/tg-collider.tsx
@@ -1,6 +1,6 @@
 import { Component, Element, h, Method,Event, ComponentInterface} from '@stencil/core';
 import { ColliderManager } from '../../manager/collider.manager';
-import { EventEmitter, Prop } from '../../../../dist/types/stencil-public-runtime';
+import { EventEmitter, Host, Prop } from '../../../../dist/types/stencil-public-runtime';
 import { ICollider } from '../../interfaces/ICollider';
 
 @Component({
@@ -9,6 +9,12 @@ import { ICollider } from '../../interfaces/ICollider';
 })
 export class TgCollider implements ComponentInterface {
   @Prop() name:string = "default";
+  @Prop() width:number = 0;
+  @Prop() height:number = 0;
+  @Prop() offsetX:number = 0;
+  @Prop() offsetY:number = 0;
+  @Prop() scale:number = 1;
+
   @Element() el: HTMLElement;
   private data: ICollider = {x:0,y:0,width:0,height:0,name:"default"};
   private manager = ColliderManager.getInstance();
@@ -17,6 +23,8 @@ export class TgCollider implements ComponentInterface {
 
   componentWillLoad() {
     this.data.name = this.name;
+    this.data.width = this.width * this.scale;
+    this.data.height = this.height * this.scale;
   }
 
   componentDidLoad() {
@@ -30,8 +38,17 @@ export class TgCollider implements ComponentInterface {
   @Method()
   async updatePosition() {
     const rect = this.el.getBoundingClientRect();
-    this.data.x =  rect.left;
-    this.data.y =  rect.top;
+    this.data.x =  rect.left + this.offsetX;
+    this.data.y =  rect.top + this.offsetY;
+  }
+
+  @Method()
+  async checkCollisionOnPosition(x:number,y:number,width:number,height:number) {
+    const a = this.data;
+    if(a.x < x + width && a.x + a.width > x &&
+      a.y < y + height && a.y + a.height > y){
+      return this.data;
+    }
   }
 
   @Method()
@@ -46,6 +63,14 @@ export class TgCollider implements ComponentInterface {
   }
 
   render() {
-    return (<slot></slot>);
+    return (<Host style={{
+      position: 'absolute',
+      top: `${this.offsetY}px`,
+      left: `${this.offsetX}px`,
+      width: `${this.width}px`,
+      height: `${this.height}px`,
+      backgroundColor: 'rgba(255,0,0,0.5)'
+    }}>
+    </Host>);
   }
 }
