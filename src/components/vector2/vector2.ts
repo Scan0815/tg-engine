5f2f7ccb58ba73bc6641e24cd279e233c4287626
oliver <oliver@bitcaster.de>
2025-04-08 15:06:43 +0200

    fix export for IVector2

---
+++ src/components/vector2/vector2.ts
@@ -0,0 +1,16 @@
+import { IVector2 } from '../interfaces/IVector2';
+
+export class Vector2 implements IVector2 {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static UP = new Vector2(0, -1);
+  static DOWN = new Vector2(0, 1);
+  static LEFT = new Vector2(-1, 0);
+  static RIGHT = new Vector2(1, 0);
+} 
